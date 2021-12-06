bc9c87dcc358a4692b4a5c4ee6184f41f548946b
riz-S <59738393+riz-S@users.noreply.github.com>
2021-12-06 17:39:32 +0800

    revisi model menyesuaikan di colab

--- model.py
+++ model.py
@@ -21,19 +21,24 @@ y = target.values
 
 
 sm = SMOTE(random_state=2)
-x_res, y_res = sm.fit_resample(x, y.ravel())
+x_res, y_res = sm.fit_resample(x, y)
 
-x_train, x_test, y_train, y_test = train_test_split(x_res, y_res, test_size= 0.2, random_state= 0)
+x_train, x_test, y_train, y_test = train_test_split(x_res, y_res , test_size=0.2, random_state=0)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=0)
 
 
 # Random Forest Classifier
 model = RandomForestClassifier()
-grid = {'criterion': ["gini", "entropy"],
+grid = {'n_estimators':[50,100], 
+        'criterion': ["gini", "entropy"],
         'min_samples_split': [2, 3, 4],
-        'min_samples_leaf': [1, 2, 3, 4]}
+        'random_state':[1,51,101]}
 clf = GridSearchCV(model, grid, scoring='accuracy')
-clf.fit(x_train, y_train)
-model = RandomForestClassifier(criterion='entropy', min_samples_leaf=1, min_samples_split=4)
+clf.fit(x_val, y_val)
+model = RandomForestClassifier(criterion = clf.best_params_['criterion'], 
+                               min_samples_split = clf.best_params_['min_samples_split'],
+                               n_estimators = clf.best_params_['n_estimators'],
+                               random_state = clf.best_params_['random_state'])
 model.fit(x_train, y_train)
 
 # Decision Tree Classifier
@@ -41,20 +46,27 @@ model_DT = DecisionTreeClassifier()
 grid_DT = {'criterion': ["gini", "entropy"],
         'splitter' : ["best", "random"],
         'min_samples_split': [2, 3, 4, 5, 6],
-        'min_samples_leaf': [1, 2, 3, 4]}
+        'random_state':[1,51,101]}
 clf_DT = GridSearchCV(model_DT, grid_DT, scoring='accuracy')
-clf_DT.fit(x_train, y_train)
-model_DT = DecisionTreeClassifier(criterion='gini', splitter='best', min_samples_leaf=1, min_samples_split=5)
+clf_DT.fit(x_val, y_val)
+model_DT = DecisionTreeClassifier(criterion = clf_DT.best_params_['criterion'],
+                                  splitter = clf_DT.best_params_['splitter'],  
+                                  min_samples_split = clf_DT.best_params_['min_samples_split'],
+                                  random_state = clf_DT.best_params_['random_state'])
 model_DT.fit(x_train, y_train)
 
 # Support Vector Machine
 model_SVC = SVC()
 grid_SVC = {'C': [0.1, 1, 10, 100],
-        'kernel' : ["rbf", "poly", "sigmoid"],
-        'gamma' : [1, 0.1, 0.01, 0.001]}
+        'kernel' : ["rbf", "poly", "sigmoid", "linear"],
+        'gamma' : [1, 0.1, 'scale', 'auto'],
+        'random_state':[1,51,101]}
 clf_SVC = GridSearchCV(model_SVC, grid_SVC, scoring='accuracy')
-clf_SVC.fit(x_train, y_train)
-model_SVC = SVC(gamma=0.1, C=10, kernel='rbf')
+clf_SVC.fit(x_val, y_val)
+model_SVC = SVC(gamma=clf_SVC.best_params_['gamma'], 
+                C=clf_SVC.best_params_['C'], 
+                kernel=clf_SVC.best_params_['kernel'],
+                random_state = clf_SVC.best_params_['random_state'])
 model_SVC.fit(x_train, y_train)
 
 pickle.dump(model, open('model_random_forest.pkl', 'wb'))
